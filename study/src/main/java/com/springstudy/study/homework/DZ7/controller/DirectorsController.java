621b020511189e96109f1dc13d10b6b63a702aec
Ewgenii <wexaskotina@yandex.ru>
2023-05-07 01:14:58 +0300

    Домашняя работа номер 8

--- study/src/main/java/com/springstudy/study/homework/DZ7/controller/DirectorsController.java
+++ study/src/main/java/com/springstudy/study/homework/DZ7/controller/DirectorsController.java
@@ -1,12 +1,20 @@
 package com.springstudy.study.homework.DZ7.controller;
 
 import com.springstudy.study.homework.DZ7.model.Directors;
+import com.springstudy.study.homework.DZ7.model.Films;
 import com.springstudy.study.homework.DZ7.repository.DirectorsRepository;
 import com.springstudy.study.homework.DZ7.repository.FilmsRepository;
 import com.springstudy.study.homework.DZ7.repository.GenericRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequestMapping("/directors")
@@ -21,5 +29,14 @@ public class DirectorsController extends GenericController <Directors> {
         super(genericRepository);
         this.directorsRepository = directorsRepository;
         this.filmsRepository = filmsRepository;
+    }
+    @Operation(description = "Добавить директора к фильмам")
+    @RequestMapping(value = "/addDirector", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Directors> addDirector(@RequestParam(value = "director_id") Long director_id,
+                                                 @RequestParam(value = "film_id") Long film_id) {
+        Directors directors = directorsRepository.findById(director_id).orElseThrow(() -> new NotFoundException("Директор не найден"));
+        Films films = filmsRepository.findById(film_id).orElseThrow(() -> new NotFoundException("Фильм не найде"));
+        directors.getFilms().add(films);
+        return ResponseEntity.status(HttpStatus.OK).body(directorsRepository.save(directors));
     }
 }
--- study/src/main/java/com/springstudy/study/homework/DZ7/controller/FilmsController.java
+++ study/src/main/java/com/springstudy/study/homework/DZ7/controller/FilmsController.java
@@ -1,20 +1,12 @@
 package com.springstudy.study.homework.DZ7.controller;
 
-import com.springstudy.study.homework.DZ7.model.Directors;
 import com.springstudy.study.homework.DZ7.model.Films;
 import com.springstudy.study.homework.DZ7.repository.DirectorsRepository;
 import com.springstudy.study.homework.DZ7.repository.FilmsRepository;
 import com.springstudy.study.homework.DZ7.repository.GenericRepository;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.webjars.NotFoundException;
 
 @RestController
 @RequestMapping("/films")
@@ -28,14 +20,4 @@ public class FilmsController extends GenericController<Films>{
         this.filmsRepository = filmsRepository;
         this.directorsRepository = directorsRepository;
     }
-    @Operation(description = "Добавить фильм к директору")
-    @RequestMapping(value = "/addDirector", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <Films> addDirector(@RequestParam(value = "director_id") Long director_id,
-                                              @RequestParam(value = "film_id") Long film_id) {
-        Directors directors = directorsRepository.findById(director_id).orElseThrow(() -> new NotFoundException("Директор не найден"));
-        Films films = filmsRepository.findById(film_id).orElseThrow(() -> new NotFoundException("Фильм не найде"));
-        films.getDirectors().add(directors);
-        return ResponseEntity.status(HttpStatus.OK).body(filmsRepository.save(films));
-    }
-
 }
