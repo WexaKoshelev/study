e44b0971dd8eb9a5735447b849e8231383fd94a3
WexaKoshelev <138188423+WexaKoshelev@users.noreply.github.com>
2023-08-12 18:40:02 +0300

    Delete Test directory

--- Test/src/HelloComponent.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class HelloComponent extends JComponent {
-    public void paintComponent (Graphics g) {
-        g.drawString("Hello, Java!", 125, 95);
-    }
-}
